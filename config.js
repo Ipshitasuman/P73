a2089763d3c0dd654b314c35bae0f55e4dc04366
Ipshitasuman <74243297+Ipshitasuman@users.noreply.github.com>
2021-11-22 10:09:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
-import * as firebase from "firebase";
-require("@firebase/firestore");
+import firebase from 'firebase'
+require('@firebase/firestore')
+import "firebase/auth";
 
   var firebaseConfig = {
-    apiKey: "AIzaSyDKuYqcZGaJOVJTl7wsVOG6EaTHNas-UOA",
-    authDomain: "cycle-app-39658.firebaseapp.com",
-    projectId: "cycle-app-39658",
-    storageBucket: "cycle-app-39658.appspot.com",
-    messagingSenderId: "432365679199",
-    appId: "1:432365679199:web:249b550bde861bf93621ea"
+
+  apiKey: "AIzaSyBjZQVgiRuMyFA50dDdq0Z6iHkXtDNMTVs",
+  authDomain: "p-73-20ab6.firebaseapp.com",
+  projectId: "p-73-20ab6",
+  storageBucket: "p-73-20ab6.appspot.com",
+  messagingSenderId: "330695962014",
+  appId: "1:330695962014:web:dabbc07cbeea98bed77a8f"
+  
   };
  
   firebase.initializeApp(firebaseConfig);
 
-export default firebase.firestore();
+  export default firebase.firestore()
+
